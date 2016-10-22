97d7ca6626ae16b41f803f6c52076f03dbf655f3
Lucian Bor <lbor01@baker.edu>
2016-10-22 01:36:06 -0400

    new form for list items

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,3 +1,4 @@
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
@@ -26,6 +27,10 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
 
 
     def check_for_row_in_list_table(self, row_text):
